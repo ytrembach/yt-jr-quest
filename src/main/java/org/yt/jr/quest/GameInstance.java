08e60edb6756251328602f1a1cdf0256057409c2
Yury Trembach <yury.trembach@gmail.com>
2024-06-20 01:36:33 +0300

    rewrite to single servlet

---
+++ src/main/java/org/yt/jr/quest/GameInstance.java
@@ -0,0 +1,49 @@
+package org.yt.jr.quest;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.yt.jr.quest.model.Game;
+import org.yt.jr.quest.model.Node;
+import java.time.LocalTime;
+import java.util.Optional;
+import java.util.UUID;
+
+@Getter
+public class GameInstance {
+
+    final private UUID id = UUID.randomUUID();
+    final private LocalTime timestamp = LocalTime.now();
+
+    final private String player;
+    final private Game game;
+
+    @Setter
+    private Node currentNode;
+
+    public GameInstance(final String player, final Game game) {
+        this.player = player;
+        this.game = game;
+        currentNode = game.getStartNode();
+    }
+
+    public Optional<Node> changeNode(final String nextNode) {
+        final UUID newNodeId = UUID.fromString(nextNode);
+        final Optional<Node> findNewNode = findNode(newNodeId);
+
+        if (findNewNode.isEmpty() ||
+                currentNode.getQuestion().getAnswers().stream()
+                .map(answer -> answer.getNextNode())
+                .noneMatch(node -> newNodeId.equals(node.getId()))) {
+            return Optional.empty();
+        }
+
+        setCurrentNode(findNewNode.get());
+        return Optional.of(currentNode);
+    }
+
+    private Optional<Node> findNode(final UUID nodeId) {
+        return game.getNodes().stream()
+                .filter(node -> node.getId().equals(nodeId))
+                .findFirst();
+    }
+}
---
+++ src/main/java/org/yt/jr/quest/KnownGames.java
@@ -0,0 +1,39 @@
+package org.yt.jr.quest;
+
+import org.yt.jr.quest.model.Demo;
+import org.yt.jr.quest.model.Game;
+
+import java.util.*;
+
+public class KnownGames {
+    private final static Map<String, Game> knownGamesMap = new HashMap<>();
+
+    private static KnownGames instance;
+
+    public static KnownGames getInstance() {
+        if (instance == null) {
+            instance = new KnownGames();
+
+            Game demoGame = new Demo().getDemo();
+            instance.registerGame(demoGame.getTitle(), demoGame);
+        }
+        return instance;
+    }
+
+    public List<String> getAllGameNames() {
+        return knownGamesMap.keySet()
+                .stream()
+                .sorted()
+                .toList();
+    }
+
+    public Optional<Game> getGame(final String gameName) {
+        return Optional.ofNullable(knownGamesMap.get(gameName));
+    }
+
+    public void registerGame(final String gameName, final Game game) {
+        knownGamesMap.put(gameName, game);
+    }
+
+
+}
