56067d80cd000630e2b3afc027de6fee451ac538
Yury Trembach <yury.trembach@gmail.com>
2024-06-30 13:39:58 +0300

    add bean classes to load game from json

---
+++ src/main/java/org/yt/jr/quest/model/gson/AnswerData.java
@@ -0,0 +1,16 @@
+package org.yt.jr.quest.model.gson;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.yt.jr.quest.model.Answer;
+
+@Getter
+@AllArgsConstructor
+public class AnswerData {
+    final private String answer;
+    final private String next;
+    final private String message;
+    @Setter
+    Answer answerBuilt;
+}
