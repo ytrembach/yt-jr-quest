0a976acd962b688796929f61fe56fe0598efefa5
Yury Trembach <yury.trembach@gmail.com>
2024-06-30 13:45:18 +0300

    add game intro add find node by name method (used when loading)

--- src/main/java/org/yt/jr/quest/model/Game.java
+++ src/main/java/org/yt/jr/quest/model/Game.java
@@ -5,21 +5,31 @@ import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Getter
 public class Game {
     final private String title;
+
+    final private String intro;
 
     final private List<Node> nodes = new ArrayList<>();
 
     @Setter
     private Node startNode;
 
-    public Game(String title) {
+    public Game(final String title, final String intro) {
         this.title = title;
+        this.intro = intro;
     }
 
-    public void addLocation(final Node loc) {
-        nodes.add(loc);
+    public void addNode(final Node node) {
+        nodes.add(node);
+    }
+
+    public Optional<Node> findByName(final String nameToSearch) {
+        return nodes.stream()
+                .filter(node -> node.getName().equals(nameToSearch))
+                .findFirst();
     }
 }
