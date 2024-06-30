b13e5dfa29eb36895b6efa1b54e22b1eefe90cad
Yury Trembach <yury.trembach@gmail.com>
2024-06-30 13:43:40 +0300

    add message when going from node to node add setter for nextNode - used when loading games

--- src/main/java/org/yt/jr/quest/GameInstance.java
+++ src/main/java/org/yt/jr/quest/GameInstance.java
@@ -5,6 +5,7 @@ import lombok.Setter;
 import org.yt.jr.quest.model.Answer;
 import org.yt.jr.quest.model.Game;
 import org.yt.jr.quest.model.Node;
+
 import java.time.LocalTime;
 import java.util.Optional;
 import java.util.UUID;
@@ -21,6 +22,9 @@ public class GameInstance {
     @Setter
     private Node currentNode;
 
+    @Setter
+    private String message = "";
+
     public GameInstance(final String player, final Game game) {
         this.player = player;
         this.game = game;
@@ -29,16 +33,18 @@ public class GameInstance {
 
     public Optional<Node> changeNode(final String nextNode) {
         final UUID newNodeId = UUID.fromString(nextNode);
-        final Optional<Node> findNewNode = findNode(newNodeId);
-
-        if (findNewNode.isEmpty() ||
-                currentNode.getQuestion().getAnswers().stream()
-                .map(Answer::getNextNode)
-                .noneMatch(node -> newNodeId.equals(node.getId()))) {
-            return Optional.empty();
+        if (!currentNode.getId().equals(newNodeId)) {
+            final Optional<Node> findNewNode = findNode(newNodeId);
+            final Optional<Answer> currAnswer = currentNode.getQuestion().getAnswers()
+                    .stream()
+                    .filter(answer -> newNodeId.equals(answer.getNextNode().getId()))
+                    .findFirst();
+            if (findNewNode.isEmpty() || currAnswer.isEmpty()) {
+                return Optional.empty();
+            }
+            setMessage(currAnswer.get().getMessage());
+            setCurrentNode(findNewNode.get());
         }
-
-        setCurrentNode(findNewNode.get());
         return Optional.of(currentNode);
     }
 
--- src/main/java/org/yt/jr/quest/model/Answer.java
+++ src/main/java/org/yt/jr/quest/model/Answer.java
@@ -2,19 +2,19 @@ package org.yt.jr.quest.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 public class Answer {
     final private String answer;
 
-    final private Node nextNode;
+    final private String message;
 
-    @Override
-    public String toString() {
-        return "Answer{" +
-                "answer='" + answer + '\'' +
-                ", nextNode=" + nextNode.getName() +
-                '}';
-    }
+    @Setter
+    @ToString.Exclude
+    private Node nextNode;
+
 }
