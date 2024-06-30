7f6739e70d82a28d99f9636c7ffbfddd5d8d8cc3
Yury Trembach <yury.trembach@gmail.com>
2024-06-30 13:48:08 +0300

    changes in demo game: - add into - add dead end node - add randomization in generating

--- src/main/java/org/yt/jr/quest/model/Demo.java
+++ src/main/java/org/yt/jr/quest/model/Demo.java
@@ -4,30 +4,34 @@ import lombok.Getter;
 
 public class Demo {
     private final String DEMO_NAME = "Test quest";
-    private final int DEMO_MAX_LOCATIONS = 10;
+    private final String DEMO_INTRO = "Test quest introduction";
+    private final int DEMO_MAX_NODES = 10;
 
     @Getter
     private final Game demo = generateDefaultGame();
 
     private Game generateDefaultGame() {
-        final Game defaultGame = new Game(DEMO_NAME);
+        final Game defaultGame = new Game(DEMO_NAME, DEMO_INTRO);
 
-        for (int i = 0; i < DEMO_MAX_LOCATIONS; i++) {
-            final boolean isFinal = i == DEMO_MAX_LOCATIONS - 1;
+        for (int i = 0; i < DEMO_MAX_NODES; i++) {
+            final boolean isFinal = i == DEMO_MAX_NODES - 1;
             final String locName = "Loc" + i;
-            final Node loc = new Node(locName, locName + " banner", isFinal);
-            defaultGame.addLocation(loc);
+            final Node loc = new Node(locName, locName, isFinal);
+            defaultGame.addNode(loc);
         }
 
-        defaultGame.setStartNode(defaultGame.getNodes().get(0));
-        Node last = defaultGame.getNodes().get(DEMO_MAX_LOCATIONS - 1);
+        final Node deadEnd = new Node("Loc fail", "You lose! Game over!", true);
+        defaultGame.addNode(deadEnd);
 
-        for (int i = 0; i < DEMO_MAX_LOCATIONS - 1; i++) {
+        defaultGame.setStartNode(defaultGame.getNodes().get(0));
+        Node last = defaultGame.getNodes().get(DEMO_MAX_NODES - 1);
+
+        for (int i = 0; i < DEMO_MAX_NODES - 1; i++) {
             final Node node = defaultGame.getNodes().get(i);
             final Node next = defaultGame.getNodes().get(i + 1);
             node.setQuestion(new Question("Q" + node.getName())
-                    .addAnswer(new Answer("Slow", next))
-                    .addAnswer(new Answer("Fast", last)));
+                    .addAnswer(new Answer("Slow", "Do next slow step", Math.random() < 0.8 ? next : deadEnd))
+                    .addAnswer(new Answer("Fast", "It was fast", last)));
         }
 
         return defaultGame;
